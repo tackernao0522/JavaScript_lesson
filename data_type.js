c4bf68ac20366bf64736bbbfa9abfd83c53500e7
takaki <cheap_trick_magic@yahoo.co.jp>
2020-12-27 19:20:48 +0900

    型の種類

---
+++ data_type.js
@@ -0,0 +1,30 @@
+// 数値型(number型)
+var num01 = 123;
+var num02 = 1.23;
+
+// 型を確認(javascriptにはnumber型の一種類しかない)
+console.log(typeof(num01));
+console.log(typeof(num02));
+
+// 文字列型(string型)
+var string_a = "Hello,World!";
+
+console.log(string_a);
+console.log(typeof(string_a));
+
+// ブール型(Boolean型) true or false
+var a = 10;
+var b = 1;
+var bool01;
+
+bool01 = (a > b);
+console.log(bool01);
+console.log(typeof(bool01));
+
+var c = 20;
+var d = 2;
+var num_bool;
+
+num_bool = (c < d);
+console.log(num_bool);
+console.log(typeof(num_bool));
