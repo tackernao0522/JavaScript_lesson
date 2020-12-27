9c95db113739dd61d542a9f06201e8eea4af9d49
takaki <cheap_trick_magic@yahoo.co.jp>
2020-12-27 19:02:39 +0900

    変数

---
+++ valiable.js
@@ -0,0 +1,22 @@
+var num = 1;
+var num01 = 2;
+var num_01 = 3;
+var num$01 = 4;
+// var num-01 = 5;
+// int 01num = 6;
+var NUM = 1;
+var Num = 2;
+// var return = 10;
+// var class = 10;
+
+
+console.log(num);
+console.log(num01);
+console.log(num_01);
+console.log(num$01);
+// console.log(num-01);
+// console.log(01num);
+console.log(NUM);
+console.log(Num);
+// console.log(return);
+// console.log(class);
